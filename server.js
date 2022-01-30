7f480be30a8499694ba25d5b6832e8cae4b72cc9
Abhay Khosla <57297897+Abhay2412@users.noreply.github.com>
2022-01-30 14:23:45 -0700

    Changed the PORT number to 80
    Which lets us use the static IP Address, and then just directly go to the server when running.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.static("public"))
-const PORT = process.env.PORT || 5000;
+const PORT = 80;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
